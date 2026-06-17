bd8abca0e0cd24b50d4c2e0592df8bcb4d69e2fc
Colin Greybosh <colingreybosh@gmail.com>
2026-06-17 08:14:39 -0700

    Small nit

--- src/config.rs
+++ src/config.rs
@@ -1,12 +1,10 @@
-use std::env;
-
 pub struct Config {
     pub base_path: String,
 }
 
 impl Config {
     pub fn parse() -> Self {
-        let base_path = match env::var("KV_DATA_DIR") {
+        let base_path = match std::env::var("KV_DATA_DIR") {
             Ok(path) => path,
             Err(_) => String::from("wal"),
         };
