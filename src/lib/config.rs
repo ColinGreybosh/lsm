0cd045b0f45d236a5645321b258eea982358fe8f
Colin Greybosh <colingreybosh@gmail.com>
2026-06-20 22:48:11 -0400

    Add tests for lib/config (#18)

--- src/lib/config.rs
+++ src/lib/config.rs
@@ -12,3 +12,35 @@ impl Config {
         Self { base_path }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::sync::Mutex;
+
+    static ENV_LOCK: Mutex<()> = Mutex::new(());
+
+    #[test]
+    fn should_get_base_path_from_environment() {
+        let _guard = ENV_LOCK.lock().unwrap();
+        let expected_path = "custom_kv_dir";
+        unsafe {
+            std::env::set_var("KV_DATA_DIR", expected_path);
+        }
+        let config = Config::parse();
+        unsafe {
+            std::env::remove_var("KV_DATA_DIR");
+        }
+        assert_eq!(config.base_path, expected_path);
+    }
+
+    #[test]
+    fn should_get_default_base_path_if_no_environment_variable() {
+        let _guard = ENV_LOCK.lock().unwrap();
+        unsafe {
+            std::env::remove_var("KV_DATA_DIR");
+        }
+        let config = Config::parse();
+        assert_eq!(config.base_path, "wal");
+    }
+}
